e407570051443b4b97cb1798eb21e7e7b5cd2f96
Gomes <t_disys.deyver@ceabs.com.br>
2023-10-17 22:29:58 -0300

    Add project files.

---
+++ EmprestimosLivros.API/Interfaces/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmprestimosLivros.API.Models;
+
+namespace EmprestimosLivros.API.Interfaces
+{
+    public interface IClienteRepository
+    {
+        void Incluir(Cliente cliente);
+        void Alterar(Cliente cliente);
+        void Excluir(int id);
+        Task<Cliente> SelecionarByPk(int id);
+        Task<IEnumerable<Cliente>> SelecionarTodos();
+    }
+}
---
+++ EmprestimosLivros.API/Repositories/ClienteRepository.cs
@@ -0,0 +1,40 @@
+﻿using EmprestimosLivros.API.Interfaces;
+using EmprestimosLivros.API.Models;
+
+namespace EmprestimosLivros.API.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ControleEmprestimoLivroContext _context;
+
+        public ClienteRepository(ControleEmprestimoLivroContext context)
+        {
+            _context = context;
+        }
+
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cliente> SelecionarByPk(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cliente>> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
